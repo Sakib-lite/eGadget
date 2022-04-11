61614b4e7dd95b614f9c54e977762c308ba6145a
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-04-12 02:11:50 +0600

    mui nextjs ssr issue fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import React, { Fragment, useEffect } from 'react';
+import '../styles/globals.css';
+import { StylesProvider, createGenerateClassName } from '@mui/styles';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'c',
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  return (
+    <Fragment>
+      <StylesProvider generateClassName={generateClassName}>
+        <Component {...pageProps} />
+      </StylesProvider>
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
