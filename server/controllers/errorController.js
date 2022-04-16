8cc415b14a29085e6774bb66d262243a426444a9
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-04-16 23:21:50 +0600

    development errors
    in development we want as much information as possible.we’ll access our environment variable and send back responses accordingly

--- server/controllers/errorController.js
+++ server/controllers/errorController.js
@@ -1,9 +1,27 @@
+const developmentError = (err, req, res) => {
+  //If any error occured in api
+  if (req.originalUrl.startsWith('/api')) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+      error: err,
+      stack: err.stack,
+    });
+  }
+
+  //for showing error in the log
+  console.log('⛔ Error:', err);
+
+  //this type of error will be rendered
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went very wrong',
+    message: err.message,
+  });
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') developmentError(err, req, res);
 };
