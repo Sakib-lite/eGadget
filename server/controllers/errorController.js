ecd6eb62437df6959428bee06e192a70ae0588af
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-04-18 00:58:19 +0600

    production error
    whenI used set NODE_ENV=production && something in npm scripts then NODE_ENV=production becomes production + " " with one whitespace after it. so i had to fixed the the bug.  process.env.NODE_ENV.trim()

--- server/controllers/errorController.js
+++ server/controllers/errorController.js
@@ -1,3 +1,5 @@
+const Error = require('./../../utils/appError')
+
 const developmentError = (err, req, res) => {
   //If any error occured in api
   if (req.originalUrl.startsWith('/api')) {
@@ -19,9 +21,46 @@ const developmentError = (err, req, res) => {
   });
 };
 
+const productionError = (err, req, res) => {
+  if (req.originalUrl.startsWith('/api')) {
+    if (err.isOperational) {
+      //this kind of errors can be shown to client
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    } else {
+      return res.status(500).json({
+        status: 'error',
+        message: 'Something went wrong',
+      });
+    }
+  }
+
+  if (err.isOperational) {
+    return res.status(res.statusCode).render('error', {
+      title: 'Something went very wrong',
+      message: err.message,
+    });
+  }
+
+  //for log
+  console.log('⛔ Error:', err);
+
+  //this type of error will be rendered also we cant send the error information to  client
+  return res.status(500).render('error', {
+    title: 'Something went very wrong',
+    message: 'Please try again',
+  });
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') developmentError(err, req, res);
+  if (process.env.NODE_ENV.trim() === 'production'){
+    productionError(err, req, res);
+  }
+
 };
