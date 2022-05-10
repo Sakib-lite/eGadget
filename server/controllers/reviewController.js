a4a510870e69f6b7c330a62e5dc0965f95022b4a
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-05-11 03:09:04 +0600

    get all reviews

--- server/controllers/reviewController.js
+++ server/controllers/reviewController.js
@@ -1,6 +1,7 @@
 const handlerController = require('./handlerController');
 const Review = require('../models/reviewModel');
 
+exports.getAllReviews=handlerController.getAllDocuments(Review);
 exports.createReview = handlerController.createDocument(Review);
 exports.getReviewById=handlerController.getDocumentById(Review)
 exports.updateReview=handlerController.updateDocument(Review)
