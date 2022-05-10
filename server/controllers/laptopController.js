25db38de79e7e5300f50a924bd6624ad627f4165
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-05-10 23:03:38 +0600

    edited

--- server/controllers/laptopController.js
+++ server/controllers/laptopController.js
@@ -1,13 +1,13 @@
 const Laptop = require('./../models/laptopModel');
 const statisticsController = require('./statisticsController');
-const productController = require('./productController');
+const productController = require('./handlerController');
 // const catchError = require('./../../utils/catchError');
 // const Error = require('../../utils/appError');
 
 
-exports.createLaptopItem = productController.createProductItem(Laptop);
-exports.getAllLaptops = productController.getAllProducts(Laptop);
-exports.getLaptop = productController.getProductById(Laptop);
-exports.updateLaptop = productController.updateProduct(Laptop);
-exports.deleteLaptop = productController.deleteProduct(Laptop);
-exports.laptopStatistics = statisticsController.productsStats(Laptop);
+exports.createLaptopItem = productController.createDocument(Laptop);
+exports.getAllLaptops = productController.getAllDocuments(Laptop);
+exports.getLaptop = productController.getDocumentById(Laptop);
+exports.updateLaptop = productController.updateDocument(Laptop);
+exports.deleteLaptop = productController.deleteDocument(Laptop);
+exports.laptopStatistics = statisticsController.productsStats(Laptop)
--- server/controllers/mobileController.js
+++ server/controllers/mobileController.js
@@ -1,11 +1,11 @@
 const Mobile = require('./../models/mobileModel');
 const statisticsController = require('./statisticsController');
-const productController = require('./productController');
+const productController = require('./handlerController');
 // const catchError = require('./../../utils/catchError');
 
-exports.createMobileItem = productController.createProductItem(Mobile);
-exports.getAllMobiles = productController.getAllProducts(Mobile);
-exports.getMobile = productController.getProductById(Mobile);
-exports.updateMobile = productController.updateProduct(Mobile);
-exports.deleteMobile = productController.deleteProduct(Mobile);
+exports.createMobileItem = productController.createDocument(Mobile);
+exports.getAllMobiles = productController.getAllDocuments(Mobile);
+exports.getMobile = productController.getDocumentById(Mobile);
+exports.updateMobile = productController.updateDocument(Mobile);
+exports.deleteMobile = productController.deleteDocument(Mobile);
 exports.mobileStatistics = statisticsController.productsStats(Mobile);
