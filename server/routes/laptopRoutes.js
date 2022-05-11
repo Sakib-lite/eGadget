ac9ea94a5425710c005f0883bd65cbeaa59a7806
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-05-11 21:06:52 +0600

    nested route for review

--- server/routes/laptopRoutes.js
+++ server/routes/laptopRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const laptopController = require('./../controllers/laptopController');
 const router = express.Router();
+const reviewRoutes= require('./reviewRoutes')
 
+
+router.use('/:productId/review',reviewRoutes) //redirected to reviewRoutes for this specific route
 router.route('/laptop-statistics').get(laptopController.laptopStatistics);
 
 router
--- server/routes/mobileRoutes.js
+++ server/routes/mobileRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const authController = require('../controllers/authController');
 const mobileController = require('./../controllers/mobileController');
 const router = express.Router();
+const reviewRoutes= require('./reviewRoutes')
 
+
+router.use('/:productId/review',reviewRoutes) //redirected to reviewRoutes for this specific route
 router.route('/mobile-statistics').get(mobileController.mobileStatistics);
 
 router
