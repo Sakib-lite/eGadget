fdb2fea22e4db4669e1036576899843b9ae7b8d7
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-07-24 01:49:25 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,7 +27,7 @@ const themeConstants = {
 };
 
 module.exports = {
-  content: [
+  purge: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './node_modules/tw-elements/dist/js/**/*.{js,ts,jsx,tsx}',
